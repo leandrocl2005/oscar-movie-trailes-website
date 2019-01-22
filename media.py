1ab81eb44002d041ab88b605995445b94a7d4a91
leandrocl2005 <leandrocl2005@yahoo.com.br>
2019-01-22 09:40:19 -0200

    Some PEP8 changes

--- media.py
+++ media.py
@@ -1,11 +1,7 @@
 class Movie(object):
 
-	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
-		self.title = title
-		self.storyline = storyline
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-
-
-
-
+    def __init__(self, title, storyline, poster_url, youtube_url):
+        self.title = title
+        self.storyline = storyline
+        self.poster_url = poster_url
+        self.youtube_url = youtube_url
